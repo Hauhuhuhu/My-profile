7d3fb9cacf41b1e1a0872b27a0e9a4e5c2098774
Hauhuhuhu <hauyhauy2005@gmail.com>
2024-08-28 08:33:44 +0700

    edit bugs + light mode

--- main.js
+++ main.js
@@ -6,13 +6,11 @@ boxBird.addEventListener('mouseenter', function(event) {
     const mouseY = event.clientY - box.top;  // Y position within the element
     const width = box.width;
     const height = box.height;
-
     const left = Math.abs(mouseX);
     const right = Math.abs(mouseX - width);
     const top = Math.abs(mouseY);
     const bottom = Math.abs(mouseY - height);
     const min = Math.min(left, right, top, bottom);
-
     if (min === left) {
         boxBird.style.transform = 'translate(400px, -600px)';
     } else if (min === right) {
@@ -43,20 +41,14 @@ setInterval(checkHover, 500);
 
 // Bỏ Chữ NCH gắn liền
 const textStrong = document.querySelector('.strong-primary');
-    function checkScreenSize() {
-        if (window.innerWidth < 739) {
-            // Thực thi đoạn mã khi cửa sổ màn hình nhỏ hơn 576px
-            // textStrong.textContent = ''; // Xóa nội dung của chữ NCH
-            textStrong.innerHTML = 'Nguyen&nbsp;Cong Hau\'s';
-            // Thực thi các hành động khác ở đây
-        }
+function checkScreenSize() {
+    if (window.innerWidth < 739) {
+        textStrong.innerHTML = 'Nguyen&nbsp;Cong Hau\'s';
     }
+}
+checkScreenSize();  // Kiểm tra ngay khi trang được tải
 
-    // Kiểm tra ngay khi trang được tải
-    checkScreenSize();
-
-    // Kiểm tra mỗi khi cửa sổ được thay đổi kích thước
-    window.addEventListener('resize', checkScreenSize);
+window.addEventListener('resize', checkScreenSize); // Kiểm tra mỗi khi cửa sổ được thay đổi kích thước
 
 // Chặn Kiểm tra trình duyệt
 document.addEventListener('contextmenu', function(event) { // Click chuột phải
@@ -71,4 +63,28 @@ window.addEventListener('keydown', function(event) {
     { // F12 U I J C
         event.preventDefault();
     }
+});
+
+// Light Mode 
+const lightMode = document.querySelectorAll('.icon-light');
+const darkMode = document.querySelectorAll('.icon-dark');
+const htmlWeb = document.querySelector('html');
+lightMode.forEach(function(lightElem) {
+    lightElem.addEventListener('click', function() {
+        htmlWeb.classList.add('light');
+        darkMode.forEach(function(darkElem) {
+            darkElem.style.display = 'block'; 
+        });
+        lightElem.style.display = 'none';
+    });
+});
+
+darkMode.forEach(function(darkElem) {
+    darkElem.addEventListener('click', function() {
+        htmlWeb.classList.remove('light');
+        lightMode.forEach(function(lightElem) {
+            lightElem.style.display = 'block';
+        });
+        darkElem.style.display = 'none';
+    });
 });
